ed3d9b6d59836868fb608f72d2f9370bf139ef1d
Pasha.xplayerx <Pasha.xplayerx@outlook.com>
2021-10-28 19:26:01 +0300

    Добавьте файлы проекта.

---
+++ Урок_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Урок_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Напиши мне своё имя");
+                String Name = Console.ReadLine();
+                Console.WriteLine(String.Format("«Привет, {0}, сегодня {1:d}»", Name, DateTime.Now)); //
+            }
+        }
+    }
+}
